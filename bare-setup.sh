2880980470c0d7b0273fc2226af18d28b6bfea08
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-09-03 21:36:59 +0530

    bare setup script added

---
+++ bare-setup.sh
@@ -0,0 +1,7 @@
+sudo pacman -S arc-gtk-theme dmenu celluloid firefox gvfs-mtp nemo nemo-fileroller nemo-terminal neofetch nitrogen ntfs-3g pamixer papirus-icon-theme pavucontrol picom pulseaudio pulsemixer scrot sxiv termite ttf-font-awesome ttf-jetbrains-mono udiskie vim wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --noconfirm
+
+cp -r .config .xinitrc /home/nebula && cd dwm-6.2
+
+sudo rm config.h && sudo make clean install && cd ..
+
+cd slstatus && sudo make clean install && cd .. && cd .. && startx
