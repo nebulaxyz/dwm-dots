457836a0a37133866e0f03a2d84f84d511c690e6
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-09-11 16:15:17 +0530

    updated script

--- bare-setup.sh
+++ bare-setup.sh
@@ -1,4 +1,6 @@
-sudo pacman -S arc-gtk-theme dmenu celluloid firefox gvfs-mtp nemo nemo-fileroller nemo-terminal neofetch nitrogen ntfs-3g pamixer papirus-icon-theme pavucontrol picom pulseaudio pulsemixer scrot sxiv termite ttf-font-awesome ttf-jetbrains-mono udiskie vim wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --noconfirm
+sudo pacman -S arc-gtk-theme i3lock-color celluloid firefox gvfs-mtp nemo nemo-fileroller nemo-terminal neofetch nitrogen ntfs-3g pamixer papirus-icon-theme pavucontrol picom pulseaudio pulsemixer rofi scrot sxiv termite ttf-font-awesome ttf-jetbrains-mono udiskie vim wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --noconfirm
+
+chmod +x .config/launch.sh
 
 cp -r .config .xinitrc /home/nebula && cd dwm-6.2
 
