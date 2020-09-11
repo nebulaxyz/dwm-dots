331fb9280f5176685db00671cabf2208c5e2d46c
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-09-11 16:15:41 +0530

    updated scripts

--- vm-setup.sh
+++ vm-setup.sh
@@ -1,4 +1,6 @@
-sudo pacman -S arc-gtk-theme dmenu celluloid firefox nemo nemo-fileroller nemo-terminal neofetch nitrogen pamixer papirus-icon-theme pavucontrol picom pulseaudio pulsemixer scrot sxiv termite ttf-font-awesome ttf-jetbrains-mono vim virtualbox-guest-utils wget xed xorg-server xorg-xinit xorg-xsetroot xorg-xrandr --noconfirm
+sudo pacman -S arc-gtk-theme i3lock-color celluloid firefox nemo nemo-fileroller nemo-terminal neofetch nitrogen pamixer papirus-icon-theme pavucontrol picom pulseaudio pulsemixer rofi scrot sxiv termite ttf-font-awesome ttf-jetbrains-mono vim virtualbox-guest-utils wget xed xorg-server xorg-xinit xorg-xsetroot xorg-xrandr --noconfirm
+
+chmod +x .config/launch.sh
 
 cp -r .config .xinitrc /home/nebula && cd dwm-6.2
 
