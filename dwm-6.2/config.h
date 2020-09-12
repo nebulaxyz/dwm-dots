318ae2e938e8b199e95d179a55281ab2f1becc4f
nebula <nebula@galaxy>
2020-09-12 15:25:20 +0530

    added gaps and removed bindings

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -44,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,8 +57,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]       = { "xfce4-terminal", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "termite", NULL };
 static const char *firefoxcmd[]    = { "firefox", NULL };
 static const char *telegramcmd[]   = { "telegram-desktop", NULL };
 static const char *vollower[]      = { "pulsemixer", "--change-volume", "-5",  NULL };
@@ -66,13 +67,12 @@ static const char *volmute[]       = { "pulsemixer", "--toggle-mute",  NULL };
 static const char *fileman[]       = { "nemo", NULL };
 static const char *scrsht[]        = { "scrot", NULL };
 static Key keys[] = {
-
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = termcmd } },
-	{ MODKEY|ControlMask,           XK_f,      spawn,          {.v = firefoxcmd } },
-    { MODKEY|ControlMask,           XK_t,      spawn,          {.v = telegramcmd } },
-    { MODKEY|ControlMask,           XK_n,      spawn,          {.v = fileman } }, 
+    { MODKEY,                       XK_f,      spawn,          {.v = firefoxcmd } },
+    { MODKEY,                       XK_t,      spawn,          {.v = telegramcmd } },
+    { MODKEY,                       XK_n,      spawn,          {.v = fileman } }, 
     { MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -82,9 +82,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_x,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -94,14 +94,18 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+
     /*volume keys and screenshoot key*/
-    { 0,                            XK_F1,     spawn,          {.v = vollower } },
-    { 0,                            XK_F2,     spawn,          {.v = volraise } },
-    { 0,                            XK_F3,     spawn,          {.v = volmute } },
-    { 0,                            XK_F5,     spawn,          {.v = scrsht } }, 
+     { 0,                            XK_F1,     spawn,          {.v = vollower } },
+     { 0,                            XK_F2,     spawn,          {.v = volraise } },
+     { 0,                            XK_F3,     spawn,          {.v = volmute } },
+     { 0,                            XK_F5,     spawn,          {.v = scrsht } }, 
     
-    TAGKEYS(                        XK_1,                      0)
+
+	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
