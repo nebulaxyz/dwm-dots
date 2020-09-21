217bbc066d82fddbb8e96470deeb619b734c8e1a
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-09-21 17:36:38 +0530

    updated package & added java_pr script

--- vm-setup.sh
+++ vm-setup.sh
@@ -1,6 +1,6 @@
-sudo pacman -S arc-gtk-theme dmenu celluloid firefox i3lock-color nemo nemo-fileroller nemo-terminal neofetch nitrogen pamixer papirus-icon-theme pavucontrol picom pulseaudio pulsemixer rofi scrot sxiv sxhkd termite ttf-font-awesome ttf-jetbrains-mono vim virtualbox-guest-utils wget xed xorg-server xorg-xinit xorg-xsetroot xorg-xrandr --noconfirm
+sudo pacman -S arc-gtk-theme wmname celluloid firefox i3lock-color nemo nemo-fileroller nemo-terminal neofetch nitrogen pamixer papirus-icon-theme pavucontrol picom pulseaudio pulsemixer rofi scrot sxiv sxhkd termite ttf-font-awesome ttf-jetbrains-mono vim virtualbox-guest-utils wget xed xorg-server xorg-xinit xorg-xsetroot xorg-xrandr --noconfirm
 
-cd .config && chmod +x down.sh launch.sh logout.sh reboot.sh && cd ..
+cd .config && chmod +x down.sh launch.sh logout.sh reboot.sh java-fix.sh && cd ..
 
 cp -r .config .xinitrc /home/nebula && cd dwm-6.2
 
