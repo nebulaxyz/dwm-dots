4e44c77a4dbf76d39d5806d21286a6ef3bc18b8b
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-10-01 22:07:24 +0530

    prevent overide installation

--- bare-setup.sh
+++ bare-setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -S arc-gtk-theme cmus wmname transmission-gtk mpv firefox gvfs-mtp i3lock-color pcmanfm-gtk3 gvfs file-roller neofetch nitrogen ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer rofi scrot sxiv sxhkd termite ttf-font-awesome ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --noconfirm
+sudo pacman -S arc-gtk-theme cmus wmname transmission-gtk mpv firefox gvfs-mtp i3lock-color pcmanfm-gtk3 gvfs file-roller neofetch nitrogen ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer rofi scrot sxiv sxhkd termite ttf-font-awesome ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
 
 cd .config && chmod +x down.sh launch.sh logout.sh reboot.sh java-fix.sh && cd ..
 
