3f80429d99605bd98596809c8bcbb017510f9362
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-11-12 00:42:02 +0530

    chromium keybind updated

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
-static const char *firefoxcmd[]    = { "chromium", NULL };
+static const char *chromiumcmd[]    = { "chromium", NULL };
 static const char *telegramcmd[]   = { "telegram-desktop", NULL };
 static const char *vollower[]      = { "pulsemixer", "--change-volume", "-5",  NULL };
 static const char *volraise[]	   = { "pulsemixer", "--change-volume", "+5",  NULL  };
@@ -70,7 +70,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = termcmd } },
-        { MODKEY,                       XK_f,      spawn,          {.v = firefoxcmd } },
+        { MODKEY,                       XK_c,      spawn,          {.v = chromiumcmd } },
         { MODKEY,                       XK_t,      spawn,          {.v = telegramcmd } },
         { MODKEY,                       XK_n,      spawn,          {.v = fileman } }, 
         { MODKEY,                       XK_b,      togglebar,      {0} },
