a324307f74ce71302bcdf58e02ac18f9a13a3fa1
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-11-11 07:58:01 +0530

    switched to chromim

--- bare-setup.sh
+++ bare-setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -S arc-gtk-theme cmus wmname transmission-gtk mpv firefox gvfs-mtp i3lock pcmanfm-gtk3 gvfs file-roller neofetch nitrogen ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer rofi scrot sxiv sxhkd termite ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
+sudo pacman -S arc-gtk-theme cmus wmname transmission-gtk mpv chromium gvfs-mtp i3lock pcmanfm-gtk3 gvfs file-roller neofetch nitrogen ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer rofi scrot sxiv sxhkd termite ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
 
 chmod +x .config/* && chmod +x .config/rofi/*
 
